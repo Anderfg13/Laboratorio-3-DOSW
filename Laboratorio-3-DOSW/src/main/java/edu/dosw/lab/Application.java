29bd18d370f7ace41f24524b6616c64b257da3d6
ignacio14.castillor@gmail.com <ignacio.castillo-r@mail.escuelaing.edu.co>
2025-08-28 14:31:33 -0500

    Parte 1 - Ignacio Castillo: Arreglo de Application.java

--- Laboratorio-3-DOSW/src/main/java/edu/dosw/lab/Application.java
+++ Laboratorio-3-DOSW/src/main/java/edu/dosw/lab/Application.java
@@ -1,5 +1,12 @@
 package edu.dosw.lab;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
-    
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("Proyecto Maven creado y corriendo correctamente 🚀");
+    }
 }
