f9f7c332392789b1fbf6588c0059f432cc5b04fb
ignacio14.castillor@gmail.com <ignacio.castillo-r@mail.escuelaing.edu.co>
2025-09-02 21:13:45 -0500

    Parte 2 - Ignacio Castillo: Reto#3 terminado

--- Laboratorio-3-DOSW/src/main/java/edu/dosw/lab/Application.java
+++ Laboratorio-3-DOSW/src/main/java/edu/dosw/lab/Application.java
@@ -2,11 +2,16 @@ package edu.dosw.lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import edu.dosw.lab.agilismo.PlanningPoker; // importa tu clase PlanningPoker
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("Proyecto Maven creado y corriendo correctamente 🚀");
+
+        // Ejecutar Planning Poker directamente
+        PlanningPoker poker = new PlanningPoker();
+        poker.main(args); // aquí llamas al método que implementa tu lógica
     }
 }
